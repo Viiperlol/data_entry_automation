929c47f7e77783ce38bcc51e1ed903db169f27e7
Viiperlol <suhaybsheikh@hotmail.com>
2025-08-24 15:20:09 +0800

    Commit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
 response = requests.get("https://appbrewery.github.io/Zillow-Clone/")
 soup = BeautifulSoup(response.text, "html.parser")
@@ -19,5 +22,25 @@ all_addresses = [address.get_text().replace(" | ", " ").strip() for address in a
 # Create a list of all the prices on the page using a CSS Selector
 all_price_elements = soup.select(".PropertyCardWrapper span")
 all_prices = [price.get_text()[:6] for price in all_price_elements if "$" in price.get_text()]
-print(f"\n After having been cleaned up, the {len(all_prices)} prices now look like this: \n")
-print(all_prices)
+# print(f"\n After having been cleaned up, the {len(all_prices)} prices now look like this: \n")
+# print(all_prices)
+
+# Set up the Selenium WebDriver (make sure to specify the correct path to your WebDriver)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=chrome_options)
+
+for n in range(len(all_links)):
+    driver.get("https://docs.google.com/forms/d/e/1FAIpQLSdug3mAwfq964KOxW88GEUBeUo8tvdHVmbAEp"
+               "0CjciBCo834Q/viewform?usp=dialog")
+    time.sleep(2)
+
+    address = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    price = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    link = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    button = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span')
+
+    address.send_keys(all_addresses[n])
+    price.send_keys(all_prices[n])
+    link.send_keys(all_links[n])
+    button.click()
